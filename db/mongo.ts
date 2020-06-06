b746e527d123f7ea218757a95229514ccfca1aa3
Farhan Hasin Chowdhury <mail@farhan.info>
2020-06-06 23:41:15 +0600

    Removed mongo

--- db/mongo.ts
+++
@@ -1,9 +0,0 @@
-import { MongoClient } from "../deps.ts";
-import env from "../config/env.ts";
-
-const client = new MongoClient();
-client.connectWithUri(env["MONGO_URL"]);
-
-const db = client.database(env["DB_DATABASE"]);
-
-export default db;
