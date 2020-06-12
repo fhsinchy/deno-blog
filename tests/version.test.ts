32cbe7df47a265f24584f3729df41172c811d062
Farhan Hasin Chowdhury <mail@farhan.info>
2020-06-13 00:20:31 +0600

    Added sample test

---
+++ tests/version.test.ts
@@ -0,0 +1,9 @@
+import { assert } from "https://deno.land/std/testing/asserts.ts";
+
+Deno.test({
+  name: "checking deno version",
+  fn(): void {
+    const version: any = Deno.env.get("DENO_VERSION");
+    assert(parseInt(version) === 1);
+  },
+});
