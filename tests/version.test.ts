fd7cacf8f77d956dcc204fd95acb0fd5fa90d072
Farhan Hasin Chowdhury <mail@farhan.info>
2020-06-13 00:23:22 +0600

    Fixed test command issue

--- tests/version.test.ts
+++ tests/version.test.ts
@@ -4,6 +4,6 @@ Deno.test({
   name: "checking deno version",
   fn(): void {
     const version: any = Deno.env.get("DENO_VERSION");
-    assert(parseInt(version) === 1);
+    assert(parseInt(version) >= 1);
   },
 });
